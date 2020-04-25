7aebe75d5fb0ae90809cee0483e8c9066d3f37df
Michał Kardaś <mkmkl93@gmail.com>
2020-04-25 16:50:54 +0200

    Added writing results

--- src/gpu-run.py
+++ src/gpu-run.py
@@ -6,12 +6,12 @@ import itertools
 
 dirPath = os.path.dirname(sys.argv[0])
 EMTDataDirPath = os.path.join(dirPath, "../../EMT6-Ro/data/")
-dataDirPath = os.path.join(dirPath, "../data/uniform_200k/protocols/")
+dataDirPath = os.path.join(dirPath, "../data/uniform_200k/")
 params = sim.load_parameters(os.path.join(EMTDataDirPath, "default-parameters.json"))
 state = sim.load_state(os.path.join(EMTDataDirPath, "tumor-lib/tumor-1.txt"), params)
 
 protocols = []
-with open(os.path.join(dataDirPath, "protocol_times_{}.csv".format(sys.argv[1])), 'r') as f:
+with open(os.path.join(dataDirPath, "protocols/protocol_times_{}.csv".format(sys.argv[1])), 'r') as f:
 	for doses, times in itertools.zip_longest(f, f):
 		protocol = []
 		doses = doses.split(' ')
@@ -24,12 +24,19 @@ with open(os.path.join(dataDirPath, "protocol_times_{}.csv".format(sys.argv[1]))
 		protocols.append(protocol)
 		break
 
+
 print(protocols)
 
 experiment = sim.Experiment(params, [state] * len(protocols), 200, len(protocols))  
 
 experiment.run(protocols)
 res = experiment.get_results()
-print("Protocol 1 result")
-print(np.mean(res[0]))
 
+if not os.path.exists(os.path.join(dataDirPath, "results")):
+    os.makedirs(os.path.join(dataDirPath, "results"))
+
+
+with open(os.path.join(dataDirPath, "results/protocol_results_{}.csv".format(sys.argv[1])), 'w') as f:
+        for i in res:
+                f.write(str(np.mean(i)))
+                f.write("\n")
