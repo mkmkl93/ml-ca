b64a703ca64ebc19cf0552cb2f3b4d05c6bac10a
Michał Kardaś <mkmkl93@gmail.com>
2020-09-26 12:07:59 +0200

    Add version of CNN for optuna usage

--- models/cnn_optuna.py
+++ models/cnn_optuna.py
@@ -7,6 +7,7 @@ from keras.models import Sequential
 from keras.optimizers import RMSprop
 import optuna
 
+import neptune
 import random
 import io
 import sys
@@ -14,6 +15,7 @@ import numpy as np
 import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
+import neptunecontrib.monitoring.optuna as opt_utils
 
 from tensorflow.keras.layers import Input, Conv1D, Activation, BatchNormalization, MaxPooling1D, Flatten, Dense, Dropout
 from tensorflow.keras.models import Model
@@ -21,14 +23,16 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import callbacks
 from absl import flags, app
 from datetime import datetime
+from neptunecontrib.monitoring.keras import NeptuneMonitor
 from data_processor import read_data
 
 flags.DEFINE_string('data_dir', '../data/uniform_200k/', 'Relative path to the data folder')
 FLAGS = flags.FLAGS
 
 
-BATCHSIZE = 16384
-EPOCHS = 5
+BATCHSIZE = 65536
+EPOCHS = 50000
+
 
 def input_fn(trainingset_path):
     x_train, y_train = read_data(trainingset_path, 'train')
@@ -42,18 +46,17 @@ def input_fn(trainingset_path):
 
 def create_cnn(trial, width, height):
     inputShape = (width, height)
-    chanDim = -1
 
-    num_of_conv_filters = trial.suggest_int("num_of_conv_filters", 1, 10)
-    num_of_dense_filters = trial.suggest_int("num_of_dense_filters", 1, 5)
-    kernel_size = trial.suggest_int("kernel_size", 1, 20)
+    num_of_conv_filters = trial.suggest_int("num_of_conv_filters", 3, 6)
+    num_of_dense_filters = trial.suggest_int("num_of_dense_filters", 1, 3)
+    kernel_size = trial.suggest_int("kernel_size", 1, 128)
 
     inputs = Input(shape=inputShape)
     x = inputs
 
     for i in range(num_of_conv_filters):
         x = Conv1D(
-                filters=trial.suggest_int("conv_filter" + str(i), 1, 512),
+                filters=trial.suggest_int("conv_filter" + str(i), 1, 192),
                 kernel_size=kernel_size,
                 strides=trial.suggest_int("strides", 1, 20),
                 activation="relu",
@@ -62,7 +65,7 @@ def create_cnn(trial, width, height):
         x = Activation("relu")(x)
         x = BatchNormalization()(x)
         x = MaxPooling1D(
-                pool_size=trial.suggest_int("pool_size" + str(i), 1, 10),
+                pool_size=trial.suggest_int("pool_size" + str(i), 1, 32),
                 padding="same"
             )(x)
 
@@ -75,7 +78,7 @@ def create_cnn(trial, width, height):
         x = Activation("relu")(x)
         x = BatchNormalization()(x)
         x = Dropout(
-                rate=trial.suggest_uniform("dense_dropout" + str(i), 0.0, 1.0)
+                rate=trial.suggest_uniform("dense_dropout" + str(i), 0.0, 0.9)
             )(x)
 
     x = Dense(1, activation="linear")(x)
@@ -86,7 +89,6 @@ def create_cnn(trial, width, height):
 
 
 def objective(trial):
-    # Clear clutter from previous Keras session graphs.
     clear_session()
 
     trainingset_path = FLAGS.data_dir
@@ -94,9 +96,8 @@ def objective(trial):
 
     model = create_cnn(trial, x_train.shape[1], 1)
 
-    # We compile our model with a sampled learning rate.
-    lr = trial.suggest_loguniform("lr", 1e-6, 1e-1)
-    decay = trial.suggest_loguniform("decay", 1e-6, 1e-1)
+    lr = trial.suggest_loguniform("lr", 1e-6, 1e-2)
+    decay = trial.suggest_loguniform("decay", 1e-6, 1e-4)
     opt = Adam(lr=lr, decay=decay)
     model.compile(
         loss=tf.keras.losses.MeanSquaredError(), optimizer=opt
@@ -109,20 +110,36 @@ def objective(trial):
         shuffle=True,
         batch_size=BATCHSIZE,
         epochs=EPOCHS,
+        callbacks=[NeptuneMonitor()],
+        verbose=0
     )
 
-    # Evaluate the model accuracy on the validation set.
     score = model.evaluate(x_valid, y_valid, verbose=0)
     return score
 
 
 def main(argv):
-    study = optuna.create_study(
-        direction="minimize"
-    )
-    two_days_in_sec = 172800 - 10 * 60 # minus 10 min
-    study.optimize(objective, n_trials=1, timeout=two_days_in_sec)
+    day_in_sec = 24 * 60 * 60 - 10 * 60  # minus 10 min
 
+    neptune.init(project_qualified_name='kowson/OLN')
+    neptune.create_experiment(name="CNN Optuna 2",
+                              tags=["CNN", "optuna", "data_v2", "mse"])
+    neptune_callback = opt_utils.NeptuneCallback(log_study=True, log_charts=True)
+
+    study = optuna.create_study(
+        study_name='cnn2',
+        direction="minimize",
+        storage='sqlite:///cnn1.db',
+        pruner=optuna.pruners.MedianPruner(n_startup_trials=0,
+                                           n_warmup_steps=10000,
+                                           interval_steps=1000),
+        load_if_exists=True
+    )
+
+    study.optimize(objective, timeout=day_in_sec, callbacks=[neptune_callback])
+    opt_utils.log_study(study)
+
+    study.set_user_attr('n_epochs', EPOCHS)
     print("Number of finished trials: {}".format(len(study.trials)))
 
     print("Best trial:")
@@ -134,9 +151,7 @@ def main(argv):
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
 
-    df = study.trials_dataframe(attrs=('number', 'value', 'params', 'state'))
-    df.to_csv('5000epochs.csv' + datetime.now().strftime("%H:%M:%S"))
-
+    neptune.stop()
 
 if __name__ == '__main__':
     app.run(main)
